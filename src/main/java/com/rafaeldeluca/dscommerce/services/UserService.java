dc1eff25538220a4561e495375e8f2d9663792d9
Rafael de Luca <deluca1712@gmail.com>
2023-08-06 21:04:27 -0300

    refactoring method authenticate

--- src/main/java/com/rafaeldeluca/dscommerce/services/UserService.java
+++ src/main/java/com/rafaeldeluca/dscommerce/services/UserService.java
@@ -2,6 +2,7 @@ package com.rafaeldeluca.dscommerce.services;
 
 import java.util.List;
 
+import com.rafaeldeluca.dscommerce.utils.CustomUserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,6 +24,9 @@ public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
+
+	@Autowired
+	private CustomUserUtil customUserUtil;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -44,9 +48,7 @@ public class UserService implements UserDetailsService {
 	
 	protected User authenticated() {
 		try {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-			String username = jwtPrincipal.getClaim("username");
+			String username = customUserUtil.getLoggedUsername();
 			return repository.findByEmail(username).get();
 		}
 		catch (Exception e) {
---
+++ src/main/java/com/rafaeldeluca/dscommerce/utils/CustomUserUtil.java
@@ -0,0 +1,18 @@
+package com.rafaeldeluca.dscommerce.utils;
+
+import org.hibernate.annotations.Comment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserUtil {
+
+    public String getLoggedUsername () {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+        String username = jwtPrincipal.getClaim("username");
+        return username;
+    }
+}
