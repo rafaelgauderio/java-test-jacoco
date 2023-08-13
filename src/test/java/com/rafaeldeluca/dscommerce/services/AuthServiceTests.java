fb37dab7115774d60437c40dadd462f9a93b11ee
Rafael de Luca <deluca1712@gmail.com>
2023-08-12 21:19:33 -0300

    test validadeSelfOrAdmin with user admin

--- src/test/java/com/rafaeldeluca/dscommerce/services/AuthServiceTests.java
+++ src/test/java/com/rafaeldeluca/dscommerce/services/AuthServiceTests.java
@@ -2,10 +2,13 @@ package com.rafaeldeluca.dscommerce.services;
 
 import com.rafaeldeluca.dscommerce.entities.User;
 import com.rafaeldeluca.dscommerce.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -22,6 +25,17 @@ public class AuthServiceTests {
         admin = UserFactory.createAdminUser();
         selClient = UserFactory.createCustomUserClient(1L,"Claúdia");
         differentClient = UserFactory.createCustomUserClient(2L,"Ana Carolina");
+    }
+
+    @Test
+    public void validateSelfOrAdminShouldNotThrowExceptionWhenUserLoggedAsAdmin () {
+        Mockito.when(userService.authenticated()).thenReturn(admin);
+
+        Long userId = admin.getId();
+
+        Assertions.assertDoesNotThrow( () -> {
+            authService.validateSelfOrAdmin(userId);
+        });
     }
 }
 ;
